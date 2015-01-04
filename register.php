6d26a3a8f3bde8aeeef4735dd622672624be7ae6
cBrahmin <angad_dubey@hotmail.com>
2015-01-04 13:27:18 -0500

    added registration form

--- register.php
+++ register.php
@@ -21,6 +21,36 @@ try {
 		<div class="col-md-12">
 			<h2>Join for Free</h2>
 		</div>
+
+		<div class="col-md-12">
+			
+			<form role="form" id="register_form">
+				
+				<div class="form-group">
+					<label for="first_name">Email address</label>
+					<input type="text" class="form-control" id="first_name" placeholder="First Name">
+				</div>
+
+				<div class="form-group">
+					<label for="last_name">Password</label>
+					<input type="text" class="form-control" id="last_name" placeholder="Last Name">
+				</div>
+
+				<div class="form-group">
+					<label for="email">Password</label>
+					<input type="email" class="form-control" id="email" placeholder="Email">
+				</div>
+
+				<div class="form-group">
+					<label for="password">Password</label>
+					<input type="password" class="form-control" id="password" placeholder="Password">
+				</div>
+
+				<button type="submit" class="btn btn-primary">Submit</button>
+
+			</form>
+
+		</div>
 	</div>
 </div>
 </body>
