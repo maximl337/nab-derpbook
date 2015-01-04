927608688a51f81360db0c7477dd39ef81f85931
cBrahmin <angad_dubey@hotmail.com>
2015-01-03 21:34:13 -0500

    got everything up

--- __install/__init.sql
+++ __install/__init.sql
@@ -1,3 +1,19 @@
 CREATE DATABASE IF NOT EXISTS `derpbook`;
 
+USE `derpbook`;
+
+CREATE TABLE IF NOT EXISTS `derpbook`.`members` (
+	`id` INT NOT NULL,
+	`first_name` VARCHAR(255) NOT NULL,
+	`last_name` VARCHAR(255) NOT NULL,
+	`email` VARCHAR(255) NOT NULL,
+	`password` VARCHAR(8) NOT NULL,
+	`avatar_url` VARCHAR(255) DEFAULT NULL,
+	`date_added` DATETIME NOT NULL,
+	PRIMARY KEY (`id`),
+	UNIQUE KEY (`email`)
+);
+
+
+
 
--- index.php
+++ index.php
@@ -5,3 +5,25 @@
 //check if user has a cookie set
 
 //display landing page if no cookie or session found
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Derpbook</title>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-12">
+				<h2> Welcome to Derpbook</h2>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
