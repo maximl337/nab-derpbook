61bd37d98e9d5e6da0f7b739dd63a63401f1a639
cBrahmin <angad_dubey@hotmail.com>
2015-01-04 19:00:27 -0500

    added more functions

--- app/controllers/_functions.php
+++ app/controllers/_functions.php
@@ -50,19 +50,55 @@ function make_username($first_name, $last_name)
 
 	$username_count = count($members);
 
+
 	if ($username_count) {
 		$username = $first_name.'-'.$last_name.'-'.$username_count;
 	} else {
 		$username = $first_name.'-'.$last_name;
 	}
 
+	return $username;
+
 }
 
 function add_member($first_name, $last_name, $email, $password)
 {
-	# code...
+
+	$dbh = get_db();
+
+	$first_name = strip_tags($first_name);
+	$last_name = strip_tags($last_name);
+	$email = strip_tags($email);
+	$password = sha1(strip_tags($password));
+
+	$sql = "INSERT INTO `members` (`first_name`, `last_name`, `email`, `password`) VALUES (:first_name, :last_name, :email, :password)";
+    $query = $dbh->prepare($sql);
+    $query->execute(array(':first_name' => $first_name, ':last_name' => $last_name, ':email' => $email, ':password' => $password));
+
+    $member_id =  $dbh->lastInsertId();
+
+    $username = make_username($first_name, $last_name);
+
+    add_username($username, $member_id);
+
+    return $member_id;
+
+
 }
 
-function add_username($username) {
+function add_username($username, $member_id) {
+
+	$dbh = get_db();
+
+	$username = strip_tags($username);
+	$member_id = strip_tags($member_id);
+
+	$sql = "UPDATE `members` SET 
+				`username` = :username
+				WHERE 
+				`id` = :member_id";
+	$query = $dbh->prepare($sql);
+	$query->execute(array(':username' => $username, ':member_id' => $member_id));
+
 
 }
